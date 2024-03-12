2a785c99e93e893cdc70e0ca348722930a84a318
Azeez Adebayo <debaycisse@gmail.com>
2024-03-12 16:25:30 -0700

    implement task 10

--- console.py
+++ console.py
@@ -42,9 +42,9 @@ class HBNBCommand(cmd.Cmd):
         """This method creates a new instance of BaseModel"""
         if not line:
             print("** class name missing **")
-        elif line != "BaseModel" and line != "User" and line != "Amenity" and
-             line != "City" and line != "Place" and line != "Review" and
-             line != "State":
+        elif (line != "BaseModel" and line != "User" and line != "Amenity" and
+              line != "City" and line != "Place" and line != "Review" and
+              line != "State"):
             print("** class doesn't exist **")
         else:
             if line == "BaseModel":
@@ -110,10 +110,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             data = line.split()
-            if data[0] != 'BaseModel' and data[0] != 'User' and
+            if (
+               data[0] != 'BaseModel' and data[0] != 'User' and
                data[0] != 'Amenity' and data[0] != 'City' and
                data[0] != 'Place' and data[0] != 'Review' and
-               data[0] != 'State':
+               data[0] != 'State'
+               ):
                 print("** class doesn't exist **")
             elif len(data) != 2:
                 print("** instance id missing **")
@@ -123,7 +125,11 @@ class HBNBCommand(cmd.Cmd):
                 bk = f'BaseModel.{data[1]}' not in all_objs.keys()
                 uk = f'User.{data[1]}' not in all_objs.keys()
                 ak = f'Amenity.{data[1]}' not in all_objs.keys()
-                if bk and uk:
+                ck = f'City.{data[1]}' not in all_objs.keys()
+                pk = f'Place.{data[1]}' not in all_objs.keys()
+                rk = f'Review.{data[1]}' not in all_objs.keys()
+                sk = f'State.{data[1]}' not in all_objs.keys()
+                if bk and uk and ak and ck and pk and rk and sk:
                     print("** no instance found **")
                 else:
                     for key in all_objs.keys():
@@ -133,6 +139,21 @@ class HBNBCommand(cmd.Cmd):
                         elif f'User.{data[1]}' == key:
                             u = User(**all_objs[key])
                             print(u)
+                        elif f'City.{data[1]}' == key:
+                            c = City(**all_objs[key])
+                            print(c)
+                        elif f'Place.{data[1]}' == key:
+                            p = Place(**all_objs[key])
+                            print(p)
+                        elif f'Amenity.{data[1]}' == key:
+                            a = Amenity(**all_objs[key])
+                            print(a)
+                        elif f'Review.{data[1]}' == key:
+                            r = Review(**all_objs[key])
+                            print(r)
+                        elif f'State.{data[1]}' == key:
+                            s = State(**all_objs[key])
+                            print(s)
 
     def do_destroy(self, line):
         """This method deletes an instance based on
@@ -145,7 +166,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             data = line.split()
-            if data[0] != 'BaseModel' and data[0] != 'User':
+            if (
+               data[0] != 'BaseModel' and data[0] != 'User' and
+               data[0] != 'Amenity' and data[0] != 'City' and
+               data[0] != 'Place' and data[0] != 'Review' and
+               data[0] != 'State'
+               ):
                 print("** class doesn't exist **")
             elif len(data) < 2:
                 print("** instance id missing **")
@@ -154,24 +180,57 @@ class HBNBCommand(cmd.Cmd):
                 all_objs = fs.all()
                 keys = [
                         f'BaseModel.{data[1]}',
-                        f'User.{data[1]}'
+                        f'User.{data[1]}',
+                        f'Amenity.{data[1]}',
+                        f'City.{data[1]}',
+                        f'Place.{data[1]}',
+                        f'Review.{data[1]}',
+                        f'State.{data[1]}'
                        ]
                 no_k0 = keys[0] not in all_objs.keys()
                 no_k1 = keys[1] not in all_objs.keys()
-                if (no_k0 and no_k1):
+                no_k2 = keys[2] not in all_objs.keys()
+                no_k3 = keys[3] not in all_objs.keys()
+                no_k4 = keys[4] not in all_objs.keys()
+                no_k5 = keys[5] not in all_objs.keys()
+                no_k6 = keys[6] not in all_objs.keys()
+                if (
+                   no_k0 and no_k1 and no_k2 and no_k3 and
+                   no_k4 and no_k5 and no_k6
+                   ):
                     print("** no instance found **")
                 else:
                     for k in all_objs.keys():
-                        if (k == keys[0]) or (k == keys[1]):
+                        if (
+                           (k == keys[0]) or (k == keys[1]) or
+                           (k == keys[2]) or (k == keys[3]) or
+                           (k == keys[4]) or (k == keys[5]) or
+                           (k == keys[6])
+                           ):
                             all_objs.pop(k)
                             break
                     for obj_key in all_objs.keys():
                         if 'BaseModel' in obj_key:
-                            c = BaseModel(**all_objs[obj_key])
-                            fs.new(c)
+                            o = BaseModel(**all_objs[obj_key])
+                            fs.new(o)
                         elif 'User' in obj_key:
-                            u = User(**all_objs[obj_key])
-                            fs.new(u)
+                            o = User(**all_objs[obj_key])
+                            fs.new(o)
+                        elif 'Amenity' in obj_key:
+                            o = Amenity(**all_objs[obj_key])
+                            fs.new(o)
+                        elif 'City' in obj_key:
+                            o = City(**all_objs[obj_key])
+                            fs.new(o)
+                        elif 'Place' in obj_key:
+                            o = Place(**all_objs[obj_key])
+                            fs.new(o)
+                        elif 'Review' in obj_key:
+                            o = Review(**all_objs[obj_key])
+                            fs.new(o)
+                        elif 'State' in obj_key:
+                            o = State(**all_objs[obj_key])
+                            fs.new(o)
                     fs.save()
 
     def do_all(self, line):
@@ -181,7 +240,11 @@ class HBNBCommand(cmd.Cmd):
         Args:
            line - the command argument, passed.
         """
-        if line and line != 'BaseModel' and line != 'User':
+        if (
+           line and line != 'BaseModel' and line != 'User' and
+           line != "City" and line != "Place" and line != 'Amenity' and
+           line != "Review" and line != "State"
+           ):
             print("** class doesn't exist **")
         else:
             data = []
@@ -193,6 +256,16 @@ class HBNBCommand(cmd.Cmd):
                         temp = User(**all_objs[key])
                     elif 'BaseModel' in key:
                         temp = BaseModel(**all_objs[key])
+                    elif 'Amenity' in key:
+                        temp = Amenity(**all_objs[key])
+                    elif 'City' in key:
+                        temp = CIty(**all_objs[key])
+                    elif 'Place' in key:
+                        temp = Place(**all_objs[key])
+                    elif 'Review' in key:
+                        temp = Review(**all_objs[key])
+                    elif 'State' in key:
+                        temp = State(**all_objs[key])
                     data.append(str(temp))
             elif line == 'User':
                 for key in all_objs.keys():
@@ -203,6 +276,31 @@ class HBNBCommand(cmd.Cmd):
                 for key in all_objs.keys():
                     if 'BaseModel' in key:
                         temp = BaseModel(**all_objs[key])
+                        data.append(str(temp))
+            elif line == 'Amenity':
+                for key in all_objs.keys():
+                    if 'Amenity' in key:
+                        temp = Amenity(**all_objs[key])
+                        data.append(str(temp))
+            elif line == 'City':
+                for key in all_objs.keys():
+                    if 'City' in key:
+                        temp = City(**all_objs[key])
+                        data.append(str(temp))
+            elif line == 'Place':
+                for key in all_objs.keys():
+                    if 'Place' in key:
+                        temp = Place(**all_objs[key])
+                        data.append(str(temp))
+            elif line == 'Review':
+                for key in all_objs.keys():
+                    if 'Review' in key:
+                        temp = Review(**all_objs[key])
+                        data.append(str(temp))
+            elif line == 'State':
+                for key in all_objs.keys():
+                    if 'State' in key:
+                        temp = State(**all_objs[key])
                         data.append(str(temp))
             print(data)
 
@@ -218,7 +316,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             args = line.split()
-            if args[0] != 'BaseModel' and args[0] != 'User':
+            if (
+               args[0] != 'BaseModel' and args[0] != 'User' and
+               args[0] != 'Amenity' and args[0] != 'City' and
+               args[0] != 'Place' and args[0] != 'Review' and
+               args[0] != 'State'
+               ):
                 print("** class doesn't exist **")
             elif not args[1]:
                 print("** instance id missing **")
@@ -226,12 +329,25 @@ class HBNBCommand(cmd.Cmd):
                 fs = storage
                 all_objs = fs.all()
                 keys = [
-                       f'BaseModel.{args[1]}',
-                       f'User.{args[1]}'
-                      ]
+                        f'BaseModel.{args[1]}',
+                        f'User.{args[1]}',
+                        f'City.{args[1]}',
+                        f'Amenity.{args[1]}',
+                        f'Place.{args[1]}',
+                        f'Review.{args[1]}',
+                        f'State.{args[1]}'
+                       ]
                 no_k0 = keys[0] not in all_objs.keys()
                 no_k1 = keys[1] not in all_objs.keys()
-                if (no_k0 and no_k1):
+                no_k2 = keys[2] not in all_objs.keys()
+                no_k3 = keys[3] not in all_objs.keys()
+                no_k4 = keys[4] not in all_objs.keys()
+                no_k5 = keys[5] not in all_objs.keys()
+                no_k6 = keys[6] not in all_objs.keys()
+                if (
+                   no_k0 and no_k1 and no_k2 and no_k3 and
+                   no_k4 and no_k5 and no_k6
+                   ):
                     print("** no instance found **")
                 else:
                     if len(args) < 3:
@@ -241,6 +357,8 @@ class HBNBCommand(cmd.Cmd):
                     else:
                         if args[3].startswith('"'):
                             args[3] = args[3][1:-1]
+                            temp = args[3]
+                            print(args[3])
                         try:
                             temp = float(args[3])
                             if str(temp) == args[3]:
@@ -255,11 +373,26 @@ class HBNBCommand(cmd.Cmd):
                             if args[1] == item_dict['id']:
                                 item_dict[args[2]] = args[3]
                                 if args[0] == 'BaseModel':
-                                    b = BaseModel(**item_dict)
-                                    fs.new(b)
+                                    o = BaseModel(**item_dict)
+                                    fs.new(o)
                                 elif args[0] == 'User':
-                                    u = User(**item_dict)
-                                    fs.new(u)
+                                    o = User(**item_dict)
+                                    fs.new(o)
+                                elif args[0] == 'City':
+                                    o = City(**item_dict)
+                                    fs.new(o)
+                                elif args[0] == 'Amenity':
+                                    o = Amenity(**item_dict)
+                                    fs.new(o)
+                                elif args[0] == 'Place':
+                                    o = Place(**item_dict)
+                                    fs.new(o)
+                                elif args[0] == 'Review':
+                                    o = Review(**item_dict)
+                                    fs.new(o)
+                                elif args[0] == 'State':
+                                    o = State(**item_dict)
+                                    fs.new(o)
                                 fs.save()
                                 break
 
